7b16088f55b0688489732402d92c16dedd5c0355
Stephen Tian <745752067@qq.com>
2018-12-18 22:47:46 +0800

    样式优化

--- 02-progress-bar/index.js
+++ 02-progress-bar/index.js
@@ -23,5 +23,14 @@ window.onload = function () {
     autoload()
   }, 45)
 
-  // 
+  // h5 progress 标签进度条
+  var h5Bar = document.getElementById('h5Progress')
+  var h5Val = setInterval(function () {
+    if (h5Bar.value != 100) {
+      h5Bar.value++
+    } else {
+      window.clearInterval(h5Val)
+      return
+    }
+  }, 100)
 }
