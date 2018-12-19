daf25ecdfbd7d5b905673d64c8bd79d7732d793d
Stephen Tian <745752067@qq.com>
2018-12-19 16:59:18 +0800

    learn webpack

---
+++ 03-webpack/content.js
@@ -0,0 +1,1 @@
+module.exports = 'It work from content.js'
---
+++ 03-webpack/entry.js
@@ -0,0 +1,10 @@
+/**
+ * author: stephentian
+ * email: stephentian@foxmail.com
+ * day: 2018-12-19
+ **/
+
+var content = require('./content.js')
+
+console.log('content.js: ', content)
+document.write('It works(entry.js)')
