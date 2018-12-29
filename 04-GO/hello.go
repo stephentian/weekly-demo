a93b615fee9996370f74172c3d96d3ecfdd2645c
Stephen Tian <745752067@qq.com>
2018-12-29 22:30:09 +0800

    go

---
+++ 04-GO/hello.go
@@ -0,0 +1,27 @@
+// 当前程序的包名
+package main
+
+// 导入的其他包
+import (
+	"fmt"
+)
+
+// 常量的定义
+const PI = 3.14
+
+// 全局变量的声明和赋值
+var name = "stephen"
+
+// 一般类型声明
+type newType int
+
+// 结构的声明
+type gopher struct{}
+
+// 接口的声明
+type golang interface{}
+
+// 由 main 函数作为程序入口点启动
+func main() {
+	fmt.Println("Hello world, Go!你好")
+}
